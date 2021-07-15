ce6edd1c84ed4372066e7f7a2dc30dbfc0430208
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-15 16:05:46 +0530

    Update Interviews.sql

--- Interviews.sql
+++ Interviews.sql
@@ -1,19 +1,20 @@
-SELECT con.contest_id, con.hacker_id, con.name, 
-SUM(sg.total_submissions), SUM(sg.total_accepted_submissions), 
-SUM(vg.total_views), SUM(vg.total_unique_views)
-FROM Contests AS con
-JOIN Colleges AS col ON con.contest_id = col.contest_id
-JOIN Challenges AS cha ON cha.college_id = col.college_id
-LEFT JOIN
-(SELECT ss.challenge_id, SUM(ss.total_submissions) AS total_submissions, SUM(ss.total_accepted_submissions) AS total_accepted_submissions FROM Submission_Stats AS ss GROUP BY ss.challenge_id) AS sg
-ON cha.challenge_id = sg.challenge_id
-LEFT JOIN
-(SELECT vs.challenge_id, SUM(vs.total_views) AS total_views, SUM(vs.total_unique_views) AS total_unique_views
-FROM View_Stats AS vs GROUP BY vs.challenge_id) AS vg
-ON cha.challenge_id = vg.challenge_id
-GROUP BY con.contest_id, con.hacker_id, con.name
-HAVING SUM(sg.total_submissions) +
-       SUM(sg.total_accepted_submissions) +
-       SUM(vg.total_views) +
-       SUM(vg.total_unique_views) > 0
-ORDER BY con.contest_id;
+select con.contest_id,
+        con.hacker_id, 
+        con.name, 
+        sum(total_submissions), 
+        sum(total_accepted_submissions), 
+        sum(total_views), sum(total_unique_views)
+from contests con 
+join colleges col on con.contest_id = col.contest_id 
+join challenges cha on  col.college_id = cha.college_id 
+left join
+(select challenge_id, sum(total_views) as total_views, sum(total_unique_views) as total_unique_views
+from view_stats group by challenge_id) vs on cha.challenge_id = vs.challenge_id 
+left join
+(select challenge_id, sum(total_submissions) as total_submissions, sum(total_accepted_submissions) as total_accepted_submissions from submission_stats group by challenge_id) ss on cha.challenge_id = ss.challenge_id
+    group by con.contest_id, con.hacker_id, con.name
+        having sum(total_submissions)!=0 or 
+                sum(total_accepted_submissions)!=0 or
+                sum(total_views)!=0 or
+                sum(total_unique_views)!=0
+            order by contest_id;
